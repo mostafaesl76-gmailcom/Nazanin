bb29181b6cf1acb9c465205a42c4d5e6a7416ea8
nazanin <nazaninhaghgou@gmail.com>
2021-10-10 12:02:05 -0700

    soal4added

---
+++ soal4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    cout<< "enter a ";
+    cin>>a ;
+    cout<<endl;
+    if (a%2==0)
+    {
+     cout<<"adad zoj ast"<< endl;
+    }
+
+    else
+
+    cout<< "adad fard ast";
+
+     
+    return 0;
+}
