2e59ffc6c5553d569a51326ef5220677a37cbb49
mosi <83770847+MostafaEsl@users.noreply.github.com>
2021-10-16 17:52:01 +0330

    edited soal10

--- soal10.cpp
+++ soal10.cpp
@@ -3,22 +3,15 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int a;
+    cout<<"enter your month number: ";
     cin>>a;
-    if(a<=12)
-    cout<<a<<endl;
-
-    if (a<6)
-        cout<<"31 day";
-        if (a>=6&&a<12)
+    if (a>=1&&a<=6)
+       cout<<"31 day";
+       else if (a>6&&a<12)
             cout<<"30 day";
                 else if(a==12)   
-             cout<<"29 day";
+                    cout<<"29 day";
                     else
-                cout<<"does not exist";
-        
-    
-
-
-
+                        cout<<"does not exist";
     return 0;
 }
